bb40a892e5596d25c02fa08b7bd1fe58eb4c5dd1
monoamine11231 <monoamine11231@gmail.com>
2023-01-11 18:56:57 +0100

    Ray generation is done on GPU. CPU creates perspective values from camera object for ray generation in GPU

--- src/cpu_ray.c
+++ src/cpu_ray.c
@@ -1,6 +1,6 @@
 #include <math.h>
 #include <float.h>
-#include <stdbool.h>
+
 #include <png.h>
 #include "cpu_ray.h"
 
@@ -38,16 +38,20 @@ void rlookat(rcamera *camera, cl_float3 dir) {
     camera->pos_dir.dir = normalize(dir);
 }
 
-rray* rgen_rays(rcamera* camera, cl_int pwidth, cl_int pheight) {
-    rray *rays;
-    cl_float3 tmp_top, forward, right, up, image_center, image_lt_corner;
-    cl_float half_radians_fov, aspect_ratio, fov_tan, image_width, image_height,
-             w_factor, h_factor;
+
+bool rgen_perspective(rcamera* camera, cl_float3* im_corner,
+                      cl_float3* camera_origin,
+                      cl_float3* up, cl_float3* right,
+                      cl_float* w_factor, cl_float* h_factor,
+                      cl_uint pwidth, cl_uint pheight) {
+
+    cl_float3 tmp_top, forward, image_center;
+    cl_float half_radians_fov, aspect_ratio, fov_tan, image_width, image_height;
     bool is_180;
 
 
+    *camera_origin = camera->pos_dir.origin;
 
-    rays                = malloc(sizeof(rray) * pwidth * pheight);
     tmp_top             = (cl_float3){.x = 0.0f, .y = 1.0f, .z = 0.0f};
     
     /* For comparison with floats and by taking in account the floating point */
@@ -55,7 +59,7 @@ rray* rgen_rays(rcamera* camera, cl_int pwidth, cl_int pheight) {
 
     /* Leave if non-acceptable fov or if tmp top vector equals the lookat pos */
     if (is_180 || camera->fov <= FLT_EPSILON || equal(tmp_top, camera->pos_dir.dir)) {
-        return NULL;
+        return false;
     }
 
     half_radians_fov    = (camera->fov / 360.0f) * M_PI;
@@ -65,8 +69,8 @@ rray* rgen_rays(rcamera* camera, cl_int pwidth, cl_int pheight) {
     image_width         = fov_tan * camera->focal_length * 2;
     image_height        = aspect_ratio * image_width;
 
-    w_factor            = image_width / pwidth;
-    h_factor            = image_height/ pheight;
+    *w_factor           = image_width / pwidth;
+    *h_factor           = image_height / pheight;
 
     /* It is normalized */
     forward             = camera->pos_dir.dir;
@@ -75,55 +79,30 @@ rray* rgen_rays(rcamera* camera, cl_int pwidth, cl_int pheight) {
     forward.z           *= -1.0f;
 
     /* Manual cross-product to construct the orthogonal unit vectors needed */
-    right               = (cl_float3){
-                            .x = tmp_top.y*forward.z-tmp_top.z*forward.y,
-                            .y = tmp_top.z*forward.x-tmp_top.x*forward.z,
-                            .z = tmp_top.x*forward.y-tmp_top.y*forward.x
+    *right              = (cl_float3){
+                        .x = tmp_top.y*forward.z-tmp_top.z*forward.y,
+                        .y = tmp_top.z*forward.x-tmp_top.x*forward.z,
+                        .z = tmp_top.x*forward.y-tmp_top.y*forward.x
                         };
-    up                  = (cl_float3){
-                            .x = forward.y*right.z-forward.z*right.y,
-                            .y = forward.z*right.x-forward.x*right.z,
-                            .z = forward.x*right.y-forward.y*right.x
+    *up                 = (cl_float3){
+                        .x = forward.y*right->z-forward.z*right->y,
+                        .y = forward.z*right->x-forward.x*right->z,
+                        .z = forward.x*right->y-forward.y*right->x
                         };
 
 
     image_center        = (cl_float3){
-                            .x = -forward.x*camera->focal_length,
-                            .y = -forward.y*camera->focal_length,
-                            .z = -forward.z*camera->focal_length
+                        .x = -forward.x*camera->focal_length,
+                        .y = -forward.y*camera->focal_length,
+                        .z = -forward.z*camera->focal_length
                         };
+
     /* Vector pointing to the left top corner of the image */
-    image_lt_corner     = (cl_float3){
-                            .x = image_center.x-right.x*image_width/2+up.x*image_height/2,
-                            .y = image_center.y-right.y*image_width/2+up.y*image_height/2,
-                            .z = image_center.z-right.z*image_width/2+up.z*image_height/2
+    *im_corner          = (cl_float3){
+                        .x = image_center.x-right->x*image_width/2+up->x*image_height/2,
+                        .y = image_center.y-right->y*image_width/2+up->y*image_height/2,
+                        .z = image_center.z-right->z*image_width/2+up->z*image_height/2
                         };
-
-    /* For every pixel cast a ray given the vector pointing to the pixel */
-    for (int w = 0; w < pwidth; w++) {
-        for (int h = 0; h < pheight; h++) {
-            rray ray;
-
-            cl_float rand_w = 1;
-            cl_float rand_h = 1;
-
-            cl_float3 vec;
-            vec.x = image_lt_corner.x+right.x*w_factor*w*rand_w-up.x*h_factor*h*rand_h;
-            vec.y = image_lt_corner.y+right.y*w_factor*w*rand_w-up.y*h_factor*h*rand_h;
-            vec.z = image_lt_corner.z+right.z*w_factor*w*rand_w-up.z*h_factor*h*rand_h;
-
-            ray.depth = 0;
-            ray.dir = normalize(vec);
-            ray.origin = camera->pos_dir.origin;
-            /* Black background on the scene */
-            ray.rgb = (cl_float3){.x = 0.0f, .y = 0.0f, .z = 1.0f};
-
-            /* Add the constructed ray to the linear list */
-            rays[h*pwidth+w] = ray;
-        }
-    }
-
-    return rays;
 }
 
 int png_dump(const char* filename, rray* rays, cl_int pwidth, cl_int pheight) {
--- src/cpu_ray.h
+++ src/cpu_ray.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <stdbool.h>
+
 #include <CL/opencl.h>
 
 
@@ -13,18 +15,26 @@ struct __rray {
 
 typedef struct __rray rray;
 
-typedef struct {
+struct __rcamera {
     rray        pos_dir;
 
     cl_float    fov;
     cl_float    focal_length;
-} rcamera;
+} __attribute__((packed));
+
+typedef struct __rcamera rcamera;
 
 
 rcamera     rinit_camera(cl_float3 camera_origin, cl_float3 camera_lookdir,
                          cl_float fov, cl_float focal_length);
 void        rlookat(rcamera *camera, cl_float3 dir);
 
-rray*       rgen_rays(rcamera* camera, cl_int pwidth, cl_int pheight);
+/* Genereates the perspective values based on the camera location and dir that
+   are essential to generate the rays on the GPU side */
+bool        rgen_perspective(rcamera* camera, cl_float3* im_corner,
+                             cl_float3* camera_origin,
+                             cl_float3* up, cl_float3* right,
+                             cl_float* w_factor, cl_float* h_factor,
+                             cl_uint pwidth, cl_uint pheight);
 
 int         png_dump(const char* filename, rray* rays, cl_int pwidth, cl_int pheight);
